d20f93463692fc22b86299754576b40c75a09344
Michsl <69900794+Michsl@users.noreply.github.com>
2022-01-29 22:03:18 +0100

    using rich

--- upload.py
+++ upload.py
@@ -1,6 +1,6 @@
 from RegisterMegaAcc import register, ExtractURL
 import clone 
-import os, sys, re, time
+import os, sys
 
 
 # upload.py <path>
@@ -16,14 +16,13 @@ import os, sys, re, time
 # file_object.close()
 
 def mega_put(path):
-    print('Uploading file...')
-    stream = os.popen(f'mega-put -c --ignore-quota-warn "{path}"')
-    output = stream.read()
-    return(output) 
+    # print('Uploading file...')
+    os.popen(f'mega-put -c --ignore-quota-warn "{path}"')
+
 
 def log(_filename, Omail, Opass, Olink, Mmail, Mpass, Mlink):
     filename = _filename.strip("\'\n")
-    print('Creating log file')
+    # print('Creating log file')
     file_object = open(f'{filename}.log', 'w')
     file_object.write(f'#### EXPORT LOG ####\nFile: {filename}\nOriginal:\n    {Omail}\n    {Opass}\n    {Olink}\nMirror:\n    {Mmail}\n    {Mpass}\n    {Mlink}')
     file_object.close()
@@ -33,7 +32,7 @@ def upload(_path, _O_email, _O_password, _M_email, _M_password):
     clone.login(_O_email, _O_password)
     mega_put(_path)
     filename = str(clone.mega_ls())
-    print(filename)
+    # print(filename)
     exported_link = ExtractURL(clone.export_file(filename))
     clone.logout()
     # time.sleep(10) i think it isnt needed
